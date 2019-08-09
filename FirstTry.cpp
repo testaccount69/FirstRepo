ca89d97ce0b238a4a612c765a19a03233e23395c
testaccount69 <mojezavib@vmailpro.net>
2019-08-08 19:00:39 -0700

    fix

--- FirstTry.cpp
+++ FirstTry.cpp
@@ -2,12 +2,6 @@
 
 int main() 
 {
-<<<<<<< HEAD
     std::cout << "Hello World" << std::endl;
     return 0;
 }
-=======
-	std::cout << "Hello World" << std::endl;
-	return 0;
-};
->>>>>>> b314cc805c4fbbef05b7e239d18a6399ae7b51db
