6eabd19a1d46733f337c3e03035dae7a565f9b11
testaccount69 <mojezavib@vmailpro.net>
2019-08-08 19:10:24 -0700

    a

--- publish.sh
+++ publish.sh
@@ -1,3 +1,9 @@
-git add . && \
-git commit . && \
-git push origin $1
+if [ -z '$1']
+then
+    echo 'forgot your branch'
+    exit 1
+else
+    git add . && \
+    git commit . && \
+    git push origin $1
+fi
